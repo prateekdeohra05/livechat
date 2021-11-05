3b3817752ce1489530f1c7059bfe437b23f0497c
prateekdeohra05 <91457161+prateekdeohra05@users.noreply.github.com>
2021-11-05 11:42:39 +0530

    Add files via upload

---
+++ nodeServer/index.js
@@ -0,0 +1,25 @@
+//Node Servver which will handle socket io connection
+
+const io = require("socket.io")(8000, {
+  cors: {
+    origin: "*",
+  },
+});
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (username) => {
+    users[socket.id] = username;
+    socket.broadcast.emit("user-joined", username);
+  });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      username: users[socket.id],
+    });
+  });
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
